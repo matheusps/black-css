0475543974aaec9c7d8560b02c5cc248c37217fd
Matheus P. Silva <matheus.ps@icloud.com>
2018-08-18 10:43:50 -0300

    Buttons, Colors, Forms, Typography and Extras. Docs already included

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var bulkSass = require('gulp-sass-bulk-import');
+
+var paths = {
+    sass: ['./src/**/*.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function (done) {
+    gulp.src('./src/blckv-css.scss')
+        .pipe(bulkSass())
+        .pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('./dist'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./dist'))
+        .on('end', done);
+});
+
+gulp.task('watch-sass', function () {
+    gulp.watch(paths.sass, ['sass']);
+});
