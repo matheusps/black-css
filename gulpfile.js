47e23b43e3a7daf9a76ca3c422506aa2abeca5a0
matheusps <cout.matheusps@gmail.com>
2019-09-09 16:59:41 -0300

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,15 @@
-const gulp = require('gulp');
-const minifyCss = require('gulp-minify-css');
-const rename = require('gulp-rename');
+let gulp = require("gulp")
+let cleanCSS = require("gulp-clean-css")
 
+gulp.task("build", () => {
+  return gulp
+    .src("src/default.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("dist"))
+})
 
-const paths = {
-    sass: ['./src/**/*.scss']
-};
-
-gulp.task('default', ['build']);
-
-gulp.task('build', function (done) {
-    gulp.src('./src/default-css.scss')
-        .pipe(bulkSass())
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(gulp.dest('./dist'))
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./dist'))
-        .on('end', done);
-});
-
-gulp.task('watch', function () {
-    gulp.watch(paths.sass, ['build', 'gen-docs']);
-});
-
-gulp.task('gen-docs', function() {
-    gulp.src('./src/default-css.scss')
-        .pipe(bulkSass())
-        .pipe(sass())
-        .on('error', sass.logError)
-        .pipe(minifyCss({
-            keepSpecialComments: 0
-        }))
-        .pipe(rename({
-            extname: '.min.css'
-        }))
-        .pipe(gulp.dest('./docs'));
-});
+gulp.task("watch", function() {
+  gulp.watch("./src/**/*.css", function(evt) {
+    gulp.task("build")
+  })
+})
