62ffc20ff66e2e939997a777ce062caf9dce031a
matheusps <cout.matheusps@gmail.com>
2019-09-26 23:19:21 -0300

    Better development

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,36 @@
-const gulp = require("gulp")
-const cleanCSS = require("gulp-clean-css")
+;(() => {
+  'use strict'
 
-gulp.task("build", () => {
-  return gulp
-    .src("src/black.css")
-    .pipe(cleanCSS())
-    .pipe(gulp.dest("dist"))
-    .pipe(gulp.dest("docs"))
-})
+  const gulp = require('gulp')
+  const cleanCSS = require('gulp-clean-css')
+  const browserSync = require('browser-sync').create()
 
-gulp.task("watch", function() {
-  gulp.watch("./src/**/*.css", function(evt) {
-    gulp.task("build")
-  })
-})
+  const serverConfig = {
+    server: {
+      baseDir: './docs',
+      index: 'index.html',
+    },
+    port: 8080,
+    open: false,
+  }
+
+  const css = () =>
+    gulp
+      .src('src/black.css')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('dist'))
+      .pipe(gulp.dest('docs'))
+
+  const watch = done => {
+    gulp.watch('./src/**/*.css', css)
+    done()
+  }
+
+  const server = done => {
+    browserSync.init(serverConfig)
+    done()
+  }
+
+  exports.build = gulp.series(css)
+  exports.default = gulp.series(css, watch, server)
+})()
